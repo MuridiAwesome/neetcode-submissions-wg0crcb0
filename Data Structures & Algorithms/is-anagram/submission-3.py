52f542eb2eb974f3924b32c46cbcf2f0dddb454a
MuridiAwesome <67024715+MuridiAwesome@users.noreply.github.com>
2026-04-02 15:57:55 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+
+        for letter in s:
+            if letter in s_map:
+                s_map[letter] += 1
+            else:
+                s_map[letter] = 1
+        
+        for letter in t:
+            if letter in t_map:
+                t_map[letter] += 1
+            else:
+                t_map[letter] = 1
+        
+        return s_map == t_map
